c1c4eb075e8dc30fecde2ded30648cacea26abd7
Taras Kushnir <tk.dev@mailbox.org>
2026-08-30 20:25:21 +0300

    Prepare for Packagist

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,19 @@
 <?php
 
-declare(strict_types=1);
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Private Captcha',
     'description' => 'Private Captcha integration for TYPO3 forms and login',
     'category' => 'services',
-    'author' => 'PrivateCaptcha',
+    'author' => 'Private Captcha',
     'state' => 'alpha',
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
+            'php' => '8.2.0-8.99.99',
             'typo3' => '13.4.0-14.99.99',
             'backend' => '13.4.0-14.99.99',
+            'extbase' => '13.4.0-14.99.99',
+            'fluid' => '13.4.0-14.99.99',
             'form' => '13.4.0-14.99.99',
             'frontend' => '13.4.0-14.99.99',
         ],
